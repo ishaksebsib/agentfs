0877c2a22d2668581765fd842f58e50f271541a0
Pekka Enberg <penberg@turso.tech>
2025-12-20 13:16:40 +0200

    AgentFS 0.4.0-pre.2

--- sdk/python/agentfs_sdk/__init__.py
+++ sdk/python/agentfs_sdk/__init__.py
@@ -8,7 +8,7 @@ from .filesystem import Filesystem, Stats
 from .kvstore import KvStore
 from .toolcalls import ToolCall, ToolCalls, ToolCallStats
 
-__version__ = "0.4.0-pre.1"
+__version__ = "0.4.0-pre.2"
 
 __all__ = [
     "AgentFS",
